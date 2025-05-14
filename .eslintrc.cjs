325caef903ee3730ac517ece100ceae48c6c1fbf
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-05-14 16:45:46 +0200

    Install and configure prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
