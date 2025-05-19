88bb508f65e400873eafb003076091c99a8e66dc
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-05-19 13:29:37 +0200

    Test addToCart() logic by simulating user clicks

--- src/components/Shop.test.jsx
+++ src/components/Shop.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import Shop from "../pages/Shop";
 import { CartProvider } from "../contexts/CartContext";
+import Navbar from "./Navbar";
 
 const mockProducts = [
   {
@@ -33,28 +35,28 @@ afterEach(() => {
 describe("Shop Page", () => {
   it("renders the Shop page title and products", async () => {
     render(
-      <MemoryRouter>
+      <MemoryRouter initialEntries={["/shop"]}>
         <CartProvider>
+          <Navbar />
           <Shop />
         </CartProvider>
       </MemoryRouter>,
     );
 
-    // Loading state
-    expect(screen.getByText(/loading products/i)).toBeInTheDocument();
-
-    // Wait for products to load
-    await waitFor(() => {
-      expect(screen.getByText("Test Product 1")).toBeInTheDocument();
-      expect(screen.getByText("Test Product 2")).toBeInTheDocument();
-    });
-
-    // Check that the heading renders
-    expect(screen.getByRole("heading", { name: /shop/i })).toBeInTheDocument();
-
-    // Check that Add to Cart buttons are present
-    expect(screen.getAllByRole("button", { name: /add to cart/i }).length).toBe(
-      2,
+    await waitFor(() =>
+      expect(screen.getByText(/Test Product 1/i)).toBeInTheDocument(),
     );
+
+    const incrementBtn = screen.getAllByRole("button", { name: "+" })[0];
+    await userEvent.click(incrementBtn);
+
+    const addToCartBtn = screen.getAllByRole("button", {
+      name: /add to cart/i,
+    })[0];
+    await userEvent.click(addToCartBtn);
+
+    await waitFor(() => {
+      expect(screen.getByText(/2 items/i)).toBeInTheDocument();
+    });
   });
 });
