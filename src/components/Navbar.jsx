e710eaefa9c1ed9cf87dfe677a4f683545cc3a13
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-05-19 16:01:36 +0200

    Remove unnecessary emoji in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar() {
             Home
           </Link>
           <Link to="/shop" data-testid="nav-shop-link">
-            🛒 My Shop
+            My Shop
           </Link>
         </div>
         <div className="flex items-center space-x-6">
